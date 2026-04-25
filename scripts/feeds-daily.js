1fcb668e741ecd4277d4bb0b88a5ace34342c263
RiiiM <riiimparm+tech@gmail.com>
2026-04-25 21:59:26 +0900

    feeds-daily.js を更新

--- scripts/feeds-daily.js
+++ scripts/feeds-daily.js
@@ -2,4 +2,5 @@ export const FEEDS = [
   { name: 'keitah', url: 'https://qiita.com/keitah/feed' },
   { name: 'UrayahaDays',   url: 'https://zenn.dev/tenormusica/feed' },
   { name: 'OpenID', url: 'https://www.openid.or.jp/blog/atom.xml' },
+  { name: 'Threat Intellingence', url: 'https://feeds.feedburner.com/threatintelligence/pvexyqv7v0v' },
 ];
